370333c7f45d85289c7e93c7f5f1adf5ee6194a4
DesDeJuan <73090527+DesDeJuan@users.noreply.github.com>
2020-11-06 16:38:55 -0700

    Cajero 1.1
    Añadi la opcion de cambio de NIP

---
+++ NIP.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+	int nip_actual = 0;
+	std::cout << "Ingrese su NIP actual: ";
+	std::cin >> nip_actual;
+}
+
--- Principal.cpp
+++ Principal.cpp
@@ -3,32 +3,58 @@
 #include <string.h>
 #include <stdio.h>
 
+//Subprogramas
 
 char datos(char usuario[15], char contra[15])                   //Esta parte captura los datos del usuario
-{                                                               //Los cuales son usuario y contraseña
+{                                                               //Los cuales son usuario y contrasenia
     std:: cout << "Ingrese su nombre de usuario" << '\n';
     std:: cin >> usuario;
 
-    std:: cout << "Ingrese su contraseña" << '\n';
+    std:: cout << "Ingrese su contrasenia" << '\n';
     std:: cin >> contra;
 
-    std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";
+}																//Fin subprograma datos de usuario
+
+void menu()
+{
+	std::cout << "Cambio de NIP ------------ 1 \n";
+	std::cout << "Consulta de saldo -------- 2 \n";
+	std::cout << "Retiro de efectivo ------- 3 \n";
+	std::cout << "Deposito ----------------- 4 \n";
+	std::cout << "Transferencia -------------5 \n";
+}
+
+char operation(char selection[1])								//Captura la seleccion del usuario para realizar una accion
+{
+	int option = 0;
+	menu();
+	std::cin >> option;
+	switch (option)
+	{
+		case 1:
+			break;
+			
+	}
 }
 
 int main()
 {
-    std:: cout << "BIENVENIDO USUARIO" << '\n';                 //Mensaje de bienvenida al usuario
+    std:: cout << "BIENVENIDO A BANACOMER" << '\n';                 //Mensaje de bienvenida al usuario
     std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
     
     std:: cout << "Ingrese sus datos para continuar" << '\n';
     char user[15] = "", contra[15] = "";                            
-    datos(user, contra);                                        //Llamada para el subprograma de datos del usuario
+    
     int attempt;
     while (attempt < 4)
     {
-        if (strcmp(user, "JUAN") && strcmp(contra, "JUANPA"))
+    	datos(user, contra);                                      //Llamada para el subprograma de datos del usuario
+    	
+        if (strcmp(user, "JUAN") == 0 && strcmp(contra, "JUANPA") == 0)
         {
-            std:: cout << "Bienvenido JUAN PABLO \n";
+            std:: cout << "Bienvenido " << user << '\n';
+            break;
         }                                         
         else
         {
@@ -36,5 +62,9 @@ int main()
             attempt++;
         }
     }
+    
+    std::cout << "*- Seleccione la operacion que desea realizar -* \n";
+    char seleccion[1] = "";
+    operation(seleccion);											//Llamada al subprograma para seleccion de operaciones
     system("PAUSE()");
 }
