e4d6c7101730382676a22711cb3fcfb13cef2f7a
DesDeJuan <73090527+DesDeJuan@users.noreply.github.com>
2020-12-08 16:37:55 -0700

    Cajero 2.0
    Agregue algunos cambios vistos en clase

--- Principal.cpp
+++ Principal.cpp
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <cstdlib>
-void menu()
+
+std::string g_user1 = "1234567890", g_user2 = "0987654321", g_user3 = "1111111111", g_nip1 = "1234", g_nip2 = "4321", g_nip3 = "1111";
+const double ticket = 20;
+void menu()															//Menu principal
 {
 	std::cout << "Cambio de NIP ------------ 1 \n";
 	std::cout << "Consulta de saldo -------- 2 \n";
@@ -12,92 +15,138 @@ void menu()
 	std::cout << "Transferencia -------------5 \n";
 }
 
-void menu_bancos()
+void menu_bancos()													//Menu de bancos
 {
-	std::cout << "BAN-ORTE ----------------------- 6 \n";
-	std::cout << "VANAMETZ ----------------------- 7 \n";
-	std::cout << "SANTA CLOS...DIGO DER ---------- 8 \n";
+	std::cout << "BANORTE ------------ 6 \n";
+	std::cout << "BANAMEX ------------ 7 \n";
+	std::cout << "SANTANDER ---------- 8 \n";
 }
 
 int main()															//CUERPO PRINCIPAL DEL PROGRAMA
 {
-	char user[15] = "", nip_actual[4] = "", nip[4] = "";
-	int banco = 0, seleccion = 0, respuesta = 1, attempt = 0;
+	std::string nip_actual, user, nip;
+	int banco = 0, seleccion = 0, respuesta = 1, attempt = 0, take = 0, ready = 0, saldo, aux;
 	unsigned int numero_cuenta = 0;
 	double retiro = 0, saldo_disp = 0, deposito = 0, transf = 0;
 	
-    std:: cout << "BIENVENIDO A BANACOMER" << '\n';                 //Mensaje de bienvenida al usuario
-    std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-    
-    while (attempt < 100)
+    std:: cout << "        BIENVENIDO A BANCOMER" << '\n';                 //Mensaje de bienvenida al usuario
+    std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    //poner while para cerrar sesion e iniciar en otra!!
+    while (ready == 0)
     {
-    	std:: cout << "*- INGRESE SUS DATOS PARA CONTINUAR -*" << '\n';  	                        
-    
-    	std:: cout << "Ingrese su nombre de usuario" << '\n';
+    	std:: cout << "*- INGRESE SUS DATOS PARA CONTINUAR -*" << '\n';              
+    	std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";
+    	
+    	std:: cout << "Numero de usuario: ";
     	std:: cin >> user;
-
-    	std:: cout << "Ingrese su NIP" << '\n';
-    	std:: cin >> nip_actual;
-
-    	std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";                                    
+    		
+    	if (user == g_user1)
+    		take = 1;
+    	else if (user == g_user2)
+    		take = 2;
+    	else if (user == g_user3)
+    		take = 3;
     	
-    	
-        if (strcmp(user, "JUAN") == 0 && strcmp(nip_actual, "1275") == 0)
-        {     	
-            std:: cout << "Bienvenido " << user << '\n';
-            break;
-        }                                         
-        else if (strcmp(user, "LAURA") == 0 && strcmp(nip_actual, "1234") == 0)
-        {
-        	std:: cout << "Bienvenido " << user << '\n';
-            break;
-        }
-        else if (strcmp(user, "PEDRO") == 0 && strcmp(nip_actual, "2323") == 0)
-        {
-        	std:: cout << "Bienvenido " << user << '\n';
-            break;
+    	switch (take)
+    	{
+    	case 1:
+    		while (nip_actual != "1234")
+    		{
+    			std::cout << "NIP: ";
+    			std::cin >> g_nip1;
+    			nip_actual = g_nip1;
+			}
+			ready = 1;
+			break;
+		case 2:
+    		while (nip_actual != "4321")
+    		{
+    			std::cout << "NIP: ";
+    			std::cin >> g_nip2;
+    			nip_actual = g_nip2;
+			}
+			ready = 1;
+			break;
+		case 3:
+    		while (nip_actual != "1111")
+    		{
+    			std::cout << "NIP: ";
+    			std::cin >> g_nip3;
+    			nip_actual = g_nip3;
+			}
+			ready = 1;
+			break;
+		default:
+			std::cout << "Usuario no encontrado!! \n";
+			system("PAUSE()");
+			system("CLS()");
+			break;
 		}
-		else
+		
+		if (ready == 1)
 		{
-			std:: cout << "Usuario incorrecto \n";
-            attempt++;
-            system("PAUSE()");
+			std:: cout << "Bienvenido: " << user << '\n';
+			system("PAUSE()");
 		}
-		system("CLS");
+		
     }
     
-    while (respuesta < 2)										//Ciclo while para repetir acciones
+    while (respuesta == 1)										//Ciclo while para repetir acciones
     {
+    	system("CLS()");
+    	
+    	std::cout << "Usuario: " << user << '\n';
+    	std::cout << "NIP: " << "****" << '\n';
+    	std::cout << '\n';
     	std::cout << "*- Seleccione la operacion que desea realizar -* \n";
+    	std::cout << '\n';
+    	
 		menu();
 		std::cin >> seleccion;
-		system("CLS");
+		
+		system("CLS()");
 	
 	switch (seleccion)
 	{
 		case 1:													//CAMBIOS DE NIP
         	
 			std::cout << "*- BIENVENIDO AL SISTEMA DE CAMBIO DE NIP -* \n";
-			while (strcmp(nip_actual, nip) != 0)
+			while (nip_actual != nip)
 			{
 				std::cout << "Por favor ingrese su NIP actual: ";
     			std::cin >> nip;
+				
+				if (nip_actual != nip)
+				{
+					std::cout << "NIP incorrecto!! Intente de nuevo";
+					system("CLS()");
+				}
+				else
+					break;	
     		}
     			std::cout << "Ingrese el NIP nuevo: ";
     			std::cin >> nip_actual;
-    
-    			std::cout << "Su nip se a actualizado a " << nip_actual << '\n';
-    			system("PAUSE");
+    			
+    			std::cout << "Su nip se ha actualizado: " << nip_actual << '\n';
+    			system("PAUSE()");
 				break;
     		
 			
 		case 2:													//CONSULTA DE SALDO
 			
 			std::cout << "*- BIENVENIDO AL SISTEMA DE CONSULTA DE SALDO -* \n";
-			std::cout << "¿Cuanto dinero desea tener? (MOMENTO XD) \n";
+			std::cout << "Saldo deseado: ";
 			std::cin >> saldo_disp;
+			std::cout << '\n';
+			
+			saldo = saldo_disp / ticket;
+			saldo = saldo * ticket;
+			aux = saldo_disp - saldo;
+			saldo_disp = saldo_disp - aux;
+			
+			
 			std::cout << "Tu saldo disponible es de: " << saldo_disp << '\n';
-			system("PAUSE");
+			system("PAUSE()");
 			break;
 			
 		case 3:													//RETIROS DE EFECTIVO
@@ -183,10 +232,10 @@ int main()															//CUERPO PRINCIPAL DEL PROGRAMA
 					std::cin >> numero_cuenta;
 					saldo_disp = saldo_disp - transf;
 					
-					if (banco == 6)																	//BANCO BAN-ORTE
+					if (banco == 6)																	//BANCO BANORTE
 				{
 					std::cout << "Transferencia realizada a: " << numero_cuenta << '\n';
-						std::cout << "Banco que recibe: BAN-ORTE \n";
+						std::cout << "Banco que recibe: BANORTE \n";
 						std::cout << "Cantidad transferida: " << transf << '\n';
 						std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";
 						std::cout << "Saldo restante en tu cuenta: " << saldo_disp << '\n';
@@ -194,8 +243,8 @@ int main()															//CUERPO PRINCIPAL DEL PROGRAMA
 				}
 				else if (banco == 7)
 				{
-					std::cout << "Transferencia realizada a: " << numero_cuenta << '\n';		//BANCO VANAMETZ
-						std::cout << "Banco que recibe: VANAMETZ \n";
+					std::cout << "Transferencia realizada a: " << numero_cuenta << '\n';		//BANCO BANAMEX
+						std::cout << "Banco que recibe: BANAMEX \n";
 						std::cout << "Cantidad transferida: " << transf << '\n';
 						std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";
 						std::cout << "Saldo restante en tu cuenta: " << saldo_disp << '\n';
@@ -203,28 +252,24 @@ int main()															//CUERPO PRINCIPAL DEL PROGRAMA
 				}
 				else
 				{
-					std::cout << "Transferencia realizada a: " << numero_cuenta << '\n';		//BANCO SANTA CLOS...DIGO DER
-						std::cout << "Banco que recibe: SANTA CLOS...DIGO DER \n";
+					std::cout << "Transferencia realizada a: " << numero_cuenta << '\n';		//BANCO SANTANDER
+						std::cout << "Banco que recibe: SANTANDER \n";
 						std::cout << "Cantidad transferida: " << transf << '\n';
 						std:: cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";
 						std::cout << "Saldo restante en tu cuenta: " << saldo_disp << '\n';
 						system("PAUSE()");
 				}
 				}
-				break;
-			
-				
-						
+				break;		
 			}
 		}
     	
     	system("CLS");
     	
-    	std::cout << "¿Desea realizar otra operacion? (1 -> SI , 2 -> NO) \n";
+    	std::cout << "ï¿½Desea realizar otra operacion? (1 -> SI , 2 -> NO) \n";
     	std::cin >> respuesta;
     	system("cls");
 	}
-    					
 	std::cout << ("*- Gracias por su visita, vuelva pronto -* \n");					
     system("PAUSE()");
 }
